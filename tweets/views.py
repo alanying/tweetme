83babe5ba14e8c954f214fda420b2b17ebda8ff8
alanying <46174760+alanying@users.noreply.github.com>
2021-10-04 19:32:07 +0100

    safe redirect

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,14 @@
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 import random
+from django.utils.http import is_safe_url
+from django.conf import settings
 
 from .models import Tweet
 from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -12,9 +16,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kewargs):
   form = TweetForm(request.POST or None)
+  next_url = request.POST.get('next') or None
   if form.is_valid():
     obj = form.save(commit=False)
     obj.save()
+    if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+      return redirect(next_url)
     form = TweetForm
   return render(request, "components/form.html",context={"form": form})
 
