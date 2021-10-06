af5204be4dcdbcc29fcb5d5d700eed9578792a36
alanying <46174760+alanying@users.noreply.github.com>
2021-10-06 18:17:46 +0100

    add tweet likes logic

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 # from tweets.models import Tweet
 
 # Register your models here.
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+class TweetLikeAdmin(admin.TabularInline):
+  model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
+  inlines = [TweetLikeAdmin]
   list_display = ['__str__', 'user']
   search_fields = ['content', 'user__username', 'user__email']
   class Meta:
